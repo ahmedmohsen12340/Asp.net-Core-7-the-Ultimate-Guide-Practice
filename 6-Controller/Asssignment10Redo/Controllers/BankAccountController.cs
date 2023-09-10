53f0c2db6d8c0a3f7b236d744624b49daac0cb85
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2023-09-11 00:06:27 +0300

    Controllers & Iaction methods Assignment 10 Redo

---
+++ 6-Controller/Asssignment10Redo/Controllers/BankAccountController.cs
@@ -0,0 +1,44 @@
+﻿using Asssignment10Redo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asssignment10Redo.Controllers
+{
+    public class BankAccountController : Controller
+    {
+        BankAccount b1 = new BankAccount() { AccountNumber = 1001, AccountHolderName = "Example Name", CurrentBalance = 5000 };
+        [Route("/")]
+        [Route("/home")]
+        public IActionResult Home()
+        {
+            return Content("Welcome to the Best Bank", "text/plain");
+        }
+        [Route("/account-details")]
+        public IActionResult AccountDetails()
+        {
+            return Json(b1);
+        }
+        [Route("/account-statement")]
+        public IActionResult AccountStatment()
+        {
+            return File("/MBA-Brochure.pdf", "application/pdf");
+        }
+        [Route("/get-current-balance/{accountNumber?}")]
+        public IActionResult Balance()
+        {
+            if (Request.RouteValues.ContainsKey("accountNumber"))
+            {
+                if (int.TryParse(Convert.ToString(Request.RouteValues["accountNumber"]), out int accNum)&& accNum == 1001)
+                {
+                    return Content($"{b1.CurrentBalance}");
+                }
+                else
+                    return BadRequest("Account Number should be 1001");
+            }
+            else
+            {
+                return NotFound("Account Number should be supplied");
+            }
+
+        }
+    }
+}
---
+++ 6-Controller/Asssignment10Redo/Models/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Asssignment10Redo.Models
+{
+    public class BankAccount
+    {
+        public int? AccountNumber { get; set; }
+        public string? AccountHolderName { get; set; }
+        public int? CurrentBalance { get; set; }
+    }
+}
