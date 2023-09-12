2db7e7e16c27d2d64cb003eb859d273157ec5f00
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2023-09-12 22:38:16 +0300

    10-Partial views Assignment 18

---
+++ 10-PartialViews/Assignment16Redo/Controllers/CitiesWeather.cs
@@ -0,0 +1,30 @@
+﻿using Assignment16Redo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment16Redo.Controllers
+{
+    public class CitiesWeatherController : Controller
+    {
+        List<Cityweather> cities = new List<Cityweather>()
+            {
+                new Cityweather(){CityUniqueCode = "LDN", CityName = "London",DateAndTime = new DateTime(2030,1,1,8,0,0), TemperatureFahrenheit = 33 },
+                new Cityweather(){CityUniqueCode = "NYC", CityName = "New York", DateAndTime = new DateTime(2030,1,1,3,0,0),TemperatureFahrenheit = 60},
+                new Cityweather(){CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = new DateTime(2030,1,1,9,0,0),  TemperatureFahrenheit = 82}
+            };
+        [Route("/weather")]
+        [Route("/")]
+        public IActionResult Home()
+        {
+
+            return View(cities);
+        }
+        [Route("/weather/{cityCode}")]
+        public IActionResult CityDetails(string? cityCode)
+        {
+            foreach(var city in cities)
+                if(cityCode == city.CityUniqueCode)
+                    return View(city);
+            return Content("Invalid City Code", "text/plain");
+        }
+    }
+}
