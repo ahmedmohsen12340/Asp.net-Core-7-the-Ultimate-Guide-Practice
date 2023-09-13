4d4f8c36fe993edc61aa1c91e9a2f1737f892562
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2023-09-13 14:35:32 +0300

    11-Component view Assignment 20

---
+++ 11-ViewComponent/Assignment20/Models/Cityweather.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment16Redo.Models
+{
+    public class Cityweather
+    {
+        public string? CityUniqueCode { get; set; }
+        public string? CityName { get; set; }
+        public DateTime? DateAndTime { get; set; }
+        public int? TemperatureFahrenheit { get; set; }
+    }
+}
---
+++ 11-ViewComponent/Assignment20/ViewComponents/CityViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Assignment16Redo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment20.ViewComponents
+{
+    [ViewComponent]
+    public class CityViewComponent: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Cityweather? x)
+        {
+            return View(x);
+        } 
+    }
+}
