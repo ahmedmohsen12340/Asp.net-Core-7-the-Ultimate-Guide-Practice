96592d35fb4f05aee4686fd2f1ebff5d35ae19b4
ahmedmohsen12340 <ahmedmohsen12340@gmail.com>
2023-10-15 09:41:39 +0300

    24-clean Archtecture follow clean archtecture in the project

---
+++ 24-Clean Archtecture/StockApp_Sln/StockApp/HelperExtentions/ConfigureServices.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Repository;
+using RepositoryContracts;
+using Serilog;
+using Services;
+using ServicesContract;
+using StockApp.ConfigurationOptions;
+
+namespace StockApp.HelperExtentions
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddAServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            //Services
+            services.AddControllersWithViews();
+            services.AddHttpClient();
+            services.AddScoped<IFinnhubGetDetailsService, FinnhubGetDetailsService>();
+            services.AddScoped<IFinnhubGetCompaniesDataService,FinnhubGetCompaniesDataService>();
+            services.AddScoped<IFinnhubSearchService, FinnhubSearchService>();
+            services.AddScoped<IFinnhubRepository, FinnhubRepository>();
+            services.AddScoped<IStocksCreateBuyOrderService, StocksCreateBuyOrderService>();
+            services.AddScoped<IStocksCreateSellOrderService, StocksCreateSellOrderService>();
+            services.AddScoped<IStocksGetBuyOrdersService, StocksGetBuyOrdersService>();
+            services.AddScoped<IStocksGetSellOrdersService, StocksGetSellOrdersService>();
+            services.AddScoped<IStocksRepository, StocksRepository>();
+            services.Configure<TradingOption>(configuration.GetSection("TradingOptions"));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+
+
+
+            services.AddHttpLogging(options =>
+            {
+                options.LoggingFields =
+                HttpLoggingFields.RequestProperties | HttpLoggingFields.ResponsePropertiesAndHeaders;
+            });
+            return services;
+        }
+    }
+}
